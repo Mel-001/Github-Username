3b5b9207f9db29581311676cbea726cdcb9cc134
meloni langat <meloni.langat@moringaschool.com>
2022-02-14 15:54:55 +0300

    added typescript to git-display

--- src/app/git-display/git-display.component.ts
+++ src/app/git-display/git-display.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
+import { GitRequestService } from '../git-http/git-request.service';
+import { User } from '../user';
+import { Repository } from '../repository';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-git-display',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GitDisplayComponent implements OnInit {
 
-  constructor() { }
+  user?: any;
+  repository:any;
+  User = environment.apiKey;
+  UserName?: string;
+   
+  constructor(public gitService:GitRequestService) {
+ 
+   }
 
-  ngOnInit(): void {
+  searchUser() {
+    this.gitService.userRequest(this.UserName);
+    
+    this.user= this.gitService.user;
+    
+    this.gitService.getUserRepos(this.UserName);
+      this.repository = this.gitService.repos
   }
 
+  userRequest(Username:any){
+    this.gitService.userRequest(Username).then(
+     ( success:any)=>{
+       this.user = this.gitService.user.login;
+     },
+    )}
+
+  getUserRepo(Username:any){
+    this.gitService.getUserRepos(Username).then(
+      ( success:any) => {
+        this.repository = this.gitService.repos
+      }
+    )} 
+
+  ngOnInit(): void {
+   
+}  
 }
+
