b400e4a197cc506601572e31fb1c5424d0a732c0
meloni langat <meloni.langat@moringaschool.com>
2022-02-14 15:48:18 +0300

    added typescript to landingpage

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { GitRequestService } from '../git-http/git-request.service';
+import { User } from '../user';
+import { Repository } from '../repository';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-landingpage',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingpageComponent implements OnInit {
 
-  constructor() { }
+  user!: any;
+  repository!: any;
+  User = environment.apiKey;
+  UserName!: string;
 
-  ngOnInit(): void {
+  constructor(public gitService:GitRequestService) { 
+    this.gitService.getUser().then(users =>{
+      this.user = users;
+      }); 
+
+      this.gitService.getRepo().then(repos =>{
+        this.repository = repos;
+      })
   }
 
+    
+  ngOnInit(): void {
+    this.gitService.getUser()
+   
+    this.user = this.gitService.user
+}    
 }
+
